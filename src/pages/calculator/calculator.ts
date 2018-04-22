a566cf3da6321e6306d2673833ac277441dc15d2
Thom Bilton <thombilton@gmail.com>
2018-04-23 10:23:33 +1200

    finished making calculator

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the CalculatorPage page.
@@ -14,8 +14,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'calculator.html',
 })
 export class CalculatorPage {
+  height: number;
+  weight: number;
+  bmiValue: number;
+  bmiMessage: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  calculateBMI() {
+    if (this.weight > 0 && this.height > 0) {
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+
+  }
+
+  //setBMIMessage will set the text nessage based on the value of BMI
+  private setBMIMessage(){
+    if(this.bmiValue < 18.5){
+      this.bmiMessage = "Underweight";
+    }
+    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
+      this.bmiMessage = "Normal"
+    }
+
+    if (this.bmiValue > 25 && this.bmiValue < 30) {
+      this.bmiMessage = "Overweight"
+    }
+
+    if (this.bmiValue > 30) {
+      this.bmiMessage = "Obese"
+    }
   }
 
   ionViewDidLoad() {
